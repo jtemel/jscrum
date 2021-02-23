618ba8aa867231a05b5d14a4ebebb741812bc857
Jonathon Temelkovski <jonathon.temelkovski@mail.utoronto.ca>
2021-02-23 09:35:56 -0500

    UTIL Add basic time util functions

---
+++ include/jscrum/util/time.hpp
@@ -0,0 +1,41 @@
+/**
+ * --
+ * @file   util/time.hpp
+ * @author Jonathon Temelkovski
+ * @desc   Provides util functions for getting and setting time.
+ * -- 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * --
+ */
+
+#ifndef __JSCRUM__UTIL__TIME_HPP__
+#define __JSCRUM__UTIL__TIME_HPP__
+
+#include <chrono>
+
+namespace jscrum {
+namespace util {
+namespace time {
+
+inline unsigned long now_ms() {
+  return milliseconds_since_epoch =
+    std::chrono::system_clock::now().time_since_epoch() / 
+    std::chrono::milliseconds(1);
+}
+
+} // namespace time
+} // namespace util
+} // namespace jscrum
+
+#endif //__JSCRUM__UTIL__TIME_HPP__
